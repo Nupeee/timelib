f480f9c3ad746bac9832b8bd006712c97eed2d94
Nupeee <rene.sass00@web.de>
2022-03-29 12:17:44 +0200

    Add existing coude

--- main.c
+++ main.c
@@ -13,12 +13,54 @@
  **/
 int day_of_the_year(int day, int month, int year)
 {
+    // Variablendeklaration
+    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int tagDesJahres = 0;
 
-    return 0;
+
+    // Prüfe um jahr Schaltjahr ist
+    if (isSchaltjahr(year))
+    {
+        tage_pro_monat[1] = 29;
+    }
+
+
+    // Schleife, die die einzelnen Tage der Monate in tagDesJahres speichert. -2, da der letzte Monat
+    // noch nicht relevant ist.
+    for (int i = 0; i <= month-2; i++){
+        tagDesJahres += tage_pro_monat[i];
+    }
+
+    // Tage des letzten Monats hinzuaddieren
+    tagDesJahres += day;
+    return tagDesJahres;
+}
+
+int isSchaltjahr(int year)
+{
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                return 1;
+
+
+            }
+            else
+                return 0; // else-Block von (jahr % 400 == 0)
+        }
+        else
+            return 1; // else-Block von (Jahr % 100 == 0)
+    }
+    else
+        return 0;  // else-Block von (Jahr % 4 == 0)
 }
 
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-    return 0;
+
 }
